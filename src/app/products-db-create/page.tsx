0f4eb4be1045201f1d665fd7ff0f369c087df19e
Sri <sri@Apple-M1.local>
2025-02-03 12:09:29 +0530

    use action state

--- src/app/products-db-create/page.tsx
+++ src/app/products-db-create/page.tsx
@@ -1,22 +1,56 @@
+'use client'
 import Submit from "@/components/submit"
 import { createProdct } from "@/prisma-db"
 import { redirect } from "next/navigation"
+import { useActionState } from "react"
 
+type Errors = {
+    title?: string;
+    description?: string;
+    price?: string;
+}
 
+type FormState = {
+    errors: Errors
+}
 
 export default function ProductsDbCreate() {
+    
+    const initialState: FormState= {
+        errors: {}
+    }
+    const [state, formAction, isPending] = useActionState(handleSubmit, initialState)
+
     
     const handleSubmit = async (formData: FormData) => {
         'use server'
         const title = await formData.get('title') as string 
         const description = await formData.get('description') as string 
         const price = await formData.get('price') as string
-        await createProdct(title, +price, description);
+
+        const errors: Errors = {}
+
+        if ( !title ) {
+            errors.title = 'Title is required'
+        }
+        if ( !description ) {
+            errors.title = 'Description is required'
+        }
+        if ( !price ) {
+            errors.title = 'Price is required'
+        }
+        
+        if ( Object.keys(errors).length > 0 ) {
+            return {errors}
+        }
+
+        await createProdct( title, +price, description );
         redirect('/products-db')
     }
   
     return (
-        <form action={handleSubmit} className="p-4 space-y-4 max-w-96">
+        <form action={formAction} className="p-4 space-y-4 max-w-96">
+
         <label className="text-white">
           Title
           <input
@@ -25,7 +59,10 @@ export default function ProductsDbCreate() {
             name="title"
            
           />
+          <br/>
+          {state.errors.title && <p className="text-red-500">{state.errors.title}</p>}
         </label>
+
         <label className="text-white">
           Price
           <input
@@ -34,7 +71,10 @@ export default function ProductsDbCreate() {
             name="price"
            
           />
+           <br/>
+           {state.errors.price && <p className="text-red-500">{state.errors.price}</p>}
         </label>
+
         <label className="text-white">
           Description
           <textarea
@@ -42,8 +82,16 @@ export default function ProductsDbCreate() {
             name="description"
             
           />
+           <br/>
+           {state.errors.description && <p className="text-red-500">{state.errors.description}</p>}
         </label>
-        <Submit/>
+
+        {/* <Submit/> */}
+
+        <button type='submit' className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500" disabled={isPending}>
+           Submit
+        </button>
+
        </form>
     )
 }
